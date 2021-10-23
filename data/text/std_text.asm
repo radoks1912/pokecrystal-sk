3d47dece495d342fcfe23c22e16d29c11edafac5
radoks1912 <radoks1912@gmail.com>
2021-10-24 00:07:48 +0200

    std_text.asm comment

--- data/text/std_text.asm
+++ data/text/std_text.asm
@@ -1,79 +1,79 @@
 NurseMornText:
-	text "Good morning!"
-	line "Welcome to our"
-	cont "#MON CENTER."
+	text "Good morning!" ; text "Good morning!"
+	line "Welcome to our" ; line "Welcome to our"
+	cont "#MON CENTER." ; cont "#MON CENTER."
 	done
 
 NurseDayText:
-	text "Hello!"
-	line "Welcome to our"
-	cont "#MON CENTER."
+	text "Hello!" ; text "Hello!"
+	line "Welcome to our" ; line "Welcome to our"
+	cont "#MON CENTER." ; cont "#MON CENTER."
 	done
 
 NurseNiteText:
-	text "Good evening!"
-	line "You're out late."
+	text "Good evening!" ; text "Good evening!"
+	line "You're out late." ; line "You're out late."
 
-	para "Welcome to our"
-	line "#MON CENTER."
+	para "Welcome to our" ; para "Welcome to our"
+	line "#MON CENTER." ; line "#MON CENTER."
 	done
 
 PokeComNurseMornText:
-	text "Good morning!"
+	text "Good morning!" ; text "Good morning!"
 
-	para "This is the #-"
-	line "MON COMMUNICATION"
+	para "This is the #-" ; para "This is the #-"
+	line "MON COMMUNICATION" ; line "MON COMMUNICATION"
 
-	para "CENTER--or the"
-	line "#COM CENTER."
+	para "CENTER--or the" ; para "CENTER--or the"
+	line "#COM CENTER." ; line "#COM CENTER."
 	done
 
 PokeComNurseDayText:
-	text "Hello!"
+	text "Hello!" ; text "Hello!"
 
-	para "This is the #-"
-	line "MON COMMUNICATION"
+	para "This is the #-" ; para "This is the #-"
+	line "MON COMMUNICATION" ; line "MON COMMUNICATION"
 
-	para "CENTER--or the"
-	line "#COM CENTER."
+	para "CENTER--or the" ; para "CENTER--or the"
+	line "#COM CENTER." ; line "#COM CENTER."
 	done
 
 PokeComNurseNiteText:
-	text "Good to see you"
-	line "working so late."
+	text "Good to see you" ; text "Good to see you"
+	line "working so late." ; line "working so late."
 
-	para "This is the #-"
-	line "MON COMMUNICATION"
+	para "This is the #-" ; para "This is the #-"
+	line "MON COMMUNICATION" ; line "MON COMMUNICATION"
 
-	para "CENTER--or the"
-	line "#COM CENTER."
+	para "CENTER--or the" ; para "CENTER--or the"
+	line "#COM CENTER." ; line "#COM CENTER."
 	done
 
 NurseAskHealText:
-	text "We can heal your"
-	line "#MON to perfect"
-	cont "health."
+	text "We can heal your" ; text "We can heal your"
+	line "#MON to perfect" ; line "#MON to perfect"
+	cont "health." ; cont "health."
 
-	para "Shall we heal your"
+	para "Shall we heal your" ; para "Shall we heal your"
 	line "#MON?"
 	done
 
 NurseTakePokemonText:
-	text "OK, may I see your"
+	text "OK, may I see your" ; text "OK, may I see your"
 	line "#MON?"
 	done
 
 NurseReturnPokemonText:
-	text "Thank you for"
-	line "waiting."
+	text "Thank you for" ; text "Thank you for"
+	line "waiting." ; line "waiting."
 
-	para "Your #MON are"
-	line "fully healed."
+	para "Your #MON are" ; para "Your #MON are"
+	line "fully healed." ; line "fully healed."
 	done
 
 NurseGoodbyeText:
-	text "We hope to see you"
-	line "again."
+	text "We hope to see you" ; text "We hope to see you"
+	line "again." ; line "again."
 	done
 
 ; not used
@@ -82,102 +82,102 @@ NurseGoodbyeText:
 	done
 
 NursePokerusText:
-	text "Your #MON"
-	line "appear to be"
+	text "Your #MON" ; text "Your #MON"
+	line "appear to be" ; line "appear to be"
 
-	para "infected by tiny"
-	line "life forms."
+	para "infected by tiny" ; para "infected by tiny"
+	line "life forms." ; line "life forms."
 
-	para "Your #MON are"
-	line "healthy and seem"
-	cont "to be fine."
+	para "Your #MON are" ; para "Your #MON are"
+	line "healthy and seem" ; line "healthy and seem"
+	cont "to be fine." ; cont "to be fine."
 
-	para "But we can't tell"
-	line "you anything more"
+	para "But we can't tell" ; para "But we can't tell"
+	line "you anything more" ; line "you anything more"
 
-	para "at a #MON"
-	line "CENTER."
+	para "at a #MON" ; para "at a #MON"
+	line "CENTER." ; line "CENTER."
 	done
 
 PokeComNursePokerusText:
-	text "Your #MON"
-	line "appear to be"
+	text "Your #MON" ; text "Your #MON"
+	line "appear to be" ; line "appear to be"
 
-	para "infected by tiny"
-	line "life forms."
+	para "infected by tiny" ; para "infected by tiny"
+	line "life forms." ; line "life forms."
 
-	para "Your #MON are"
-	line "healthy and seem"
-	cont "to be fine."
+	para "Your #MON are" ; para "Your #MON are"
+	line "healthy and seem" ; line "healthy and seem"
+	cont "to be fine." ; cont "to be fine."
 
-	para "But we can't tell"
-	line "you anything more."
+	para "But we can't tell" ; para "But we can't tell"
+	line "you anything more." ; line "you anything more."
 	done
 
 DifficultBookshelfText:
-	text "It's full of"
-	line "difficult books."
+	text "It's full of" ; text "It's full of"
+	line "difficult books." ; line "difficult books."
 	done
 
 PictureBookshelfText:
-	text "A whole collection"
-	line "of #MON picture"
-	cont "books!"
+	text "A whole collection" ; text "A whole collection"
+	line "of #MON picture" ; line "of #MON picture"
+	cont "books!" ; cont "books!"
 	done
 
 MagazineBookshelfText:
-	text "#MON magazines…"
-	line "#MON PAL,"
+	text "#MON magazines…" ; text "#MON magazines…"
+	line "#MON PAL," ; line "#MON PAL,"
 
-	para "#MON HANDBOOK,"
-	line "#MON GRAPH…"
+	para "#MON HANDBOOK," ; para "#MON HANDBOOK,"
+	line "#MON GRAPH…" ; line "#MON GRAPH…"
 	done
 
 TeamRocketOathText:
-	text "TEAM ROCKET OATH"
+	text "TEAM ROCKET OATH" ; text "TEAM ROCKET OATH"
 
-	para "Steal #MON for"
-	line "profit!"
+	para "Steal #MON for" ; para "Steal #MON for"
+	line "profit!" ; line "profit!"
 
-	para "Exploit #MON"
-	line "for profit!"
+	para "Exploit #MON" ; para "Exploit #MON"
+	line "for profit!" ; line "for profit!"
 
-	para "All #MON exist"
-	line "for the glory of"
-	cont "TEAM ROCKET!"
+	para "All #MON exist" ; para "All #MON exist"
+	line "for the glory of" ; line "for the glory of"
+	cont "TEAM ROCKET!" ; cont "TEAM ROCKET!"
 	done
 
 IncenseBurnerText:
-	text "What is this?"
+	text "What is this?" ; text "What is this?"
 
-	para "Oh, it's an"
-	line "incense burner!"
+	para "Oh, it's an" ; para "Oh, it's an"
+	line "incense burner!" ; line "incense burner!"
 	done
 
 MerchandiseShelfText:
-	text "Lots of #MON"
-	line "merchandise!"
+	text "Lots of #MON" ; text "Lots of #MON"
+	line "merchandise!" ; line "merchandise!"
 	done
 
 LookTownMapText:
-	text "It's the TOWN MAP."
+	text "It's the TOWN MAP." ; text "It's the TOWN MAP."
 	done
 
 WindowText:
-	text "My reflection!"
-	line "Lookin' good!"
+	text "My reflection!" ; text "My reflection!"
+	line "Lookin' good!" ; line "Lookin' good!"
 	done
 
 TVText:
-	text "It's a TV."
+	text "It's a TV." ; text "It's a TV."
 	done
 
 HomepageText:
-	text "#MON JOURNAL"
-	line "HOME PAGE…"
+	text "#MON JOURNAL" ; text "#MON JOURNAL"
+	line "HOME PAGE…" ; line "HOME PAGE…"
 
-	para "It hasn't been"
-	line "updated…"
+	para "It hasn't been" ; para "It hasn't been"
+	line "updated…" ; line "updated…"
 	done
 
 ; not used
@@ -188,94 +188,94 @@ HomepageText:
 	done
 
 TrashCanText:
-	text "There's nothing in"
-	line "here…"
+	text "There's nothing in" ; text "There's nothing in"
+	line "here…" ; line "here…"
 	done
 
 ; not used
-	text "A #MON may be"
-	line "able to move this."
+	text "A #MON may be" ; text "A #MON may be"
+	line "able to move this." ; line "able to move this."
 	done
 
 ; not used
-	text "Maybe a #MON"
-	line "can break this."
+	text "Maybe a #MON" ; text "Maybe a #MON"
+	line "can break this." ; line "can break this."
 	done
 
 PokecenterSignText:
-	text "Heal Your #MON!"
+	text "Heal Your #MON!" ; text "Heal Your #MON!"
 	line "#MON CENTER"
 	done
 
 MartSignText:
-	text "For All Your"
-	line "#MON Needs"
+	text "For All Your" ; text "For All Your"
+	line "#MON Needs" ; line "#MON Needs"
 
-	para "#MON MART"
+	para "#MON MART" ; para "#MON MART"
 	done
 
 ContestResults_ReadyToJudgeText:
-	text "We will now judge"
-	line "the #MON you've"
-	cont "caught."
+	text "We will now judge" ; text "We will now judge"
+	line "the #MON you've" ; line "the #MON you've"
+	cont "caught." ; cont "caught."
 
 	para "<……>"
 	line "<……>"
 
-	para "We have chosen the"
-	line "winners!"
+	para "We have chosen the" ; para "We have chosen the"
+	line "winners!" ; line "winners!"
 
-	para "Are you ready for"
-	line "this?"
+	para "Are you ready for" ; para "Are you ready for"
+	line "this?" ; line "this?"
 	done
 
 ContestResults_PlayerWonAPrizeText:
-	text "<PLAYER>, the No.@"
+	text "<PLAYER>, the No.@" ; text "<PLAYER>, the No.@"
 	text_ram wStringBuffer3
 	text_start
-	line "finisher, wins"
+	line "finisher, wins" ; line "finisher, wins"
 	cont "@"
 	text_ram wStringBuffer4
 	text "!"
 	done
 
 ReceivedItemText:
-	text "<PLAYER> received"
+	text "<PLAYER> received" ; text "<PLAYER> received"
 	line "@"
 	text_ram wStringBuffer4
 	text "."
 	done
 
 ContestResults_JoinUsNextTimeText:
-	text "Please join us for"
-	line "the next Contest!"
+	text "Please join us for" ; text "Please join us for"
+	line "the next Contest!" ; line "the next Contest!"
 	done
 
 ContestResults_ConsolationPrizeText:
-	text "Everyone else gets"
-	line "a BERRY as a con-"
-	cont "solation prize!"
+	text "Everyone else gets" ; text "Everyone else gets"
+	line "a BERRY as a con-" ; line "a BERRY as a con-"
+	cont "solation prize!" ; cont "solation prize!"
 	done
 
 ContestResults_DidNotWinText:
-	text "We hope you do"
-	line "better next time."
+	text "We hope you do" ; text "We hope you do"
+	line "better next time." ; line "better next time."
 	done
 
 ContestResults_ReturnPartyText:
-	text "We'll return the"
-	line "#MON we kept"
+	text "We'll return the" ; text "We'll return the"
+	line "#MON we kept" ; line "#MON we kept"
 
-	para "for you."
-	line "Here you go!"
+	para "for you." ; para "for you."
+	line "Here you go!" ; line "Here you go!"
 	done
 
 ContestResults_PartyFullText:
-	text "Your party's full,"
-	line "so the #MON was"
+	text "Your party's full," ; text "Your party's full,"
+	line "so the #MON was" ; line "so the #MON was"
 
-	para "sent to your BOX"
-	line "in BILL's PC."
+	para "sent to your BOX" ; para "sent to your BOX"
+	line "in BILL's PC." ; line "in BILL's PC."
 	done
 
 GymStatue_CityGymText:
@@ -288,101 +288,101 @@ GymStatue_WinningTrainersText:
 	text "LEADER: @"
 	text_ram wStringBuffer4
 	text_start
-	para "WINNING TRAINERS:"
+	para "WINNING TRAINERS:" ; para "WINNING TRAINERS:"
 	line "<PLAYER>"
 	done
 
 CoinVendor_WelcomeText:
-	text "Welcome to the"
-	line "GAME CORNER."
+	text "Welcome to the" ; text "Welcome to the"
+	line "GAME CORNER." ; line "GAME CORNER."
 	done
 
 CoinVendor_NoCoinCaseText:
-	text "Do you need game"
-	line "coins?"
+	text "Do you need game" ; text "Do you need game"
+	line "coins?" ; line "coins?"
 
-	para "Oh, you don't have"
-	line "a COIN CASE for"
-	cont "your coins."
+	para "Oh, you don't have" ; para "Oh, you don't have"
+	line "a COIN CASE for" ; line "a COIN CASE for"
+	cont "your coins." ; cont "your coins."
 	done
 
 CoinVendor_IntroText:
-	text "Do you need some"
-	line "game coins?"
+	text "Do you need some" ; text "Do you need some"
+	line "game coins?" ; line "game coins?"
 
-	para "It costs ¥1000 for"
-	line "50 coins. Do you"
-	cont "want some?"
+	para "It costs ¥1000 for" ; para "It costs ¥1000 for"
+	line "50 coins. Do you" ; line "50 coins. Do you"
+	cont "want some?" ; cont "want some?"
 	done
 
 CoinVendor_Buy50CoinsText:
-	text "Thank you!"
-	line "Here are 50 coins."
+	text "Thank you!" ; text "Thank you!"
+	line "Here are 50 coins." ; line "Here are 50 coins."
 	done
 
 CoinVendor_Buy500CoinsText:
-	text "Thank you! Here"
-	line "are 500 coins."
+	text "Thank you! Here" ; text "Thank you! Here"
+	line "are 500 coins." ; line "are 500 coins."
 	done
 
 CoinVendor_NotEnoughMoneyText:
-	text "You don't have"
-	line "enough money."
+	text "You don't have" ; text "You don't have"
+	line "enough money." ; line "enough money."
 	done
 
 CoinVendor_CoinCaseFullText:
-	text "Whoops! Your COIN"
-	line "CASE is full."
+	text "Whoops! Your COIN" ; text "Whoops! Your COIN"
+	line "CASE is full." ; line "CASE is full."
 	done
 
 CoinVendor_CancelText:
-	text "No coins for you?"
-	line "Come again!"
+	text "No coins for you?" ; text "No coins for you?"
+	line "Come again!" ; line "Come again!"
 	done
 
 BugContestPrizeNoRoomText:
-	text "Oh? Your PACK is"
-	line "full."
+	text "Oh? Your PACK is" ; text "Oh? Your PACK is"
+	line "full." ; line "full."
 
-	para "We'll keep this"
-	line "for you today, so"
+	para "We'll keep this" ; para "We'll keep this"
+	line "for you today, so" ; line "for you today, so"
 
-	para "come back when you"
-	line "make room for it."
+	para "come back when you" ; para "come back when you"
+	line "make room for it." ; line "make room for it."
 	done
 
 HappinessText3:
-	text "Wow! You and your"
-	line "#MON are really"
-	cont "close!"
+	text "Wow! You and your" ; text "Wow! You and your"
+	line "#MON are really" ; line "#MON are really"
+	cont "close!" ; cont "close!"
 	done
 
 HappinessText2:
-	text "#MON get more"
-	line "friendly if you"
+	text "#MON get more" ; text "#MON get more"
+	line "friendly if you" ; line "friendly if you"
 
-	para "spend time with"
-	line "them."
+	para "spend time with" ; para "spend time with"
+	line "them." ; line "them."
 	done
 
 HappinessText1:
-	text "You haven't tamed"
-	line "your #MON."
+	text "You haven't tamed" ; text "You haven't tamed"
+	line "your #MON." ; line "your #MON."
 
-	para "If you aren't"
-	line "nice, it'll pout."
+	para "If you aren't" ; para "If you aren't"
+	line "nice, it'll pout." ; line "nice, it'll pout."
 	done
 
 RegisteredNumber1Text:
-	text "<PLAYER> registered"
+	text "<PLAYER> registered" ; text "<PLAYER> registered"
 	line "@"
 	text_ram wStringBuffer3
-	text "'s number."
+	text "'s number." ; text "'s number."
 	done
 
 RegisteredNumber2Text:
-	text "<PLAYER> registered"
+	text "<PLAYER> registered" ; text "<PLAYER> registered"
 	line "@"
 	text_ram wStringBuffer3
-	text "'s number."
+	text "'s number." ; text "'s number."
 	done
